5d740b83d829947b6914427b6c34a50893fa4b5c
Keiko Oda <keiko713@gmail.com>
2022-02-28 10:57:08 -0800

    Follow before-fork after-fork with unicorn

--- app/config/unicorn.rb
+++ app/config/unicorn.rb
@@ -2,7 +2,13 @@ worker_processes 1
 timeout 15
 preload_app true
 
-before_fork do |server, worker|
-end
 after_fork do |server, worker|
+  Mongoid::Clients.clients.each do |name, client|
+    client.close
+    client.reconnect
+  end
+end
+
+before_fork do |server, worker|
+  Mongoid.disconnect_clients
 end
